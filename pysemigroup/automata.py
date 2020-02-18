48b46424d844f5e15a19ce4e60b9906c89af53ce
Charles Paperman <charles.paperman@inria.fr>
2020-02-18 11:50:57 +0100

    fix matrix to boolean

--- pysemigroup/automata.py
+++ pysemigroup/automata.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-import subprocess   
+import subprocess
 from copy import copy
 import os
 import numpy as np
@@ -12,7 +12,7 @@ def CartesianProduct_aut(A,B):
     transitions = {}
     alphabet = []
     for a in alpha1:
-        alphabet.append(a[0]+"#"+a[1])    
+        alphabet.append(a[0]+"#"+a[1])
     for x in states:
         for a in alphabet:
             transitions[(tuple(x),a)] = [(A._transitions[(x[0],a[0])][0],B._transitions[(x[1],a[2])][0])]
@@ -24,7 +24,7 @@ def CartesianProduct_aut(A,B):
     for x in A._final_states:
         for y in B._final_states:
             final_states.append((x,y))
-    return Automaton(transitions,initial_states,final_states,alphabet=alphabet)    
+    return Automaton(transitions,initial_states,final_states,alphabet=alphabet)
 
 def random_automaton(size, alphabet):
     states = range(size)
@@ -33,8 +33,8 @@ def random_automaton(size, alphabet):
         for a in alphabet:
             transition[(x,a)] = sample(states,1)
     return Automaton(transition,sample(states,1),sample(states,1),states,alphabet)
-    
-    
+
+
 class Automaton(object):
     def __init__(self, transitions, initial_states, final_states, states=None, alphabet=None, aut_type="boolean"):
         r"""
@@ -92,7 +92,7 @@ class Automaton(object):
         automaton
 
         EXAMPLES::
-        
+
             sage: from pysemigroup import Automaton
             sage: A = Automaton.from_letter('a')
             sage: A
@@ -109,7 +109,7 @@ class Automaton(object):
         transitions = {(0,letter):[1]}
         if alphabet:
             return Automaton(transitions, initial_states, final_states,alphabet=alphabet)
-        else:    
+        else:
             return Automaton(transitions, initial_states, final_states,alphabet=set([letter]))
     def rename_letters(self, dic):
         alphabet = set()
@@ -125,7 +125,7 @@ class Automaton(object):
         self._alphabet = alphabet
         self._transitions = transitions
 
-    @classmethod    
+    @classmethod
     def from_empty_string(Automaton,letters):
         r"""
 
@@ -140,13 +140,13 @@ class Automaton(object):
         EXAMPLES::
         """
         initial_states = [0]
-        final_states = [0]   
-        transitions = {}     
+        final_states = [0]
+        transitions = {}
         for a in letters:
             transitions[(0,a)] = [1]
         return Automaton(transitions, initial_states, final_states)
-        
-        
+
+
     @classmethod
     def from_theirs(Automaton, their):
         r"""
@@ -160,10 +160,10 @@ class Automaton(object):
 
         Automaton
 
-        EXAMPLES:: 
-       
+        EXAMPLES::
+
             sage: from pysemigroup import Automaton
-            sage: d = {('p','a'):'q',('q','a'):'p',('p','b'):'p',('q','b'):'q'}     
+            sage: d = {('p','a'):'q',('q','a'):'p',('p','b'):'p',('q','b'):'q'}
             sage: A = Automaton(d,['p'],['q'])
             sage: B = A.to_theirs()
             sage: B
@@ -184,14 +184,14 @@ class Automaton(object):
              letter = t.word_in[0]
              transitions[(stateA,letter)].append(stateB)
         return Automaton(transitions, initial_states, final_states)
-       
-        
+
+
     def to_sage_automaton(self):
         r"""
         Return Sage version of this automaton.
 
         EXAMPLES::
-        
+
             sage: from pysemigroup import Automaton
             sage: d = {('p','a'):'q',('q','a'):'p',('p','b'):'p',('q','b'):'q'}
             sage: A = Automaton(d,['p'],['q'])
@@ -204,13 +204,13 @@ class Automaton(object):
             ['p']
             sage: B.final_states()
             ['q']
-            
+
         Non deterministic example::
-            
+
             sage: d = {('p','a'):['q','p'],('q','a'):'p',('p','b'):'p'}
             sage: A = Automaton(d,['p'],['q'])
             sage: B = A.to_theirs()
-            sage: B  
+            sage: B
             Automaton with 2 states
 
 
@@ -228,7 +228,7 @@ class Automaton(object):
     def save(self,save_to_file,extension="svg"):
         r"""
         """
-        save_graphviz(self.graphviz_string(),save_to_file,extension=extension)            
+        save_graphviz(self.graphviz_string(),save_to_file,extension=extension)
     def __repr__(self):
         r"""
         String representation.
@@ -245,7 +245,7 @@ class Automaton(object):
         return s
     def __pos__(self):
         return self
-        
+
     def __add__(self, other):
         r"""
         Return an automaton that compute the union of self and other.
@@ -302,7 +302,7 @@ class Automaton(object):
             final_states.append(d[(x,1)])
 
         return Automaton(transitions, initial_states, final_states,alphabet=alphabet)
-         
+
     def __mul__(self, other):
         r"""
         Return an automaton that compute the concatenation of self and other.
@@ -379,7 +379,7 @@ class Automaton(object):
         EXAMPLES::
         """
 
-   
+
         A = self.deterministic_automaton()
         final_states = []
         for i in (A._states - set(A._final_states)):
@@ -391,22 +391,22 @@ class Automaton(object):
 
         OUTPUT:
 
-        automaton 
+        automaton
 
         EXAMPLE::
 
         """
         return (-((-(self))+(-(other))))
 
- 
 
-        
+
+
     def __sub__(self, other):
         r"""
 
         OUTPUT:
 
-        automaton 
+        automaton
 
         """
         return  self.intersection(-(other))
@@ -461,7 +461,7 @@ class Automaton(object):
         return Automaton(transitions, self._initial_states, final_states,alphabet=self._alphabet)
     def __xor__(self,exponent):
         return self.__pow__(exponent)
-    
+
     def __pow__(self, exponent):
         r"""
         Return power of self.
@@ -624,7 +624,7 @@ class Automaton(object):
             sage: from pysemigroup import Automaton
             sage: d = {('p', 'a'): 'p', ('p', 'b'): ['q', 'p'], (frozenset(['a']), 'a'): 'b'}
             sage: A = Automaton(d,['p'],['q'])
-            sage: sorted(A._states) 
+            sage: sorted(A._states)
             [frozenset({'a'}), 'b', 'p', 'q']
             sage: A.rename_states()
             sage: sorted(A._states)
@@ -649,24 +649,24 @@ class Automaton(object):
         self._final_states = final_states
         self._transitions = transitions
         self._states = states
-        
+
     def is_finite_state_reachable(self):
         r"""
-        Return whether the 
+        Return whether the
 
         OUTPUT:
 
         Boolean
 
         EXAMPLES::
-        
+
             sage: from pysemigroup import Automaton
             sage: d = {('p', 'a'): ['p'], ('p', 'b'): ['p', 'q']}
             sage: A = Automaton(d, ['p'], ['q'])
             sage: A.is_finite_state_reachable()
             True
 
-        """    
+        """
         if len(set(self._initial_states).intersection(set(self._final_states))) > 0:
             return True
         to_test = set(self._initial_states)
@@ -681,8 +681,8 @@ class Automaton(object):
                             to_test.add(b)
                         if b in self._final_states:
                             return True
-        return False                                        
-    
+        return False
+
     def deterministic_automaton(self, rename_states=False):
         r"""
         Return a deterministic version of the automaton by the powerset method.
@@ -761,42 +761,42 @@ class Automaton(object):
         - ``rename_states`` -- bool (default: True),
 
         EXAMPLES::
-        
+
             sage: from pysemigroup import Automaton
             sage: d = {('p', 'a'): ['p'], ('p', 'b'): ['p', 'q']}
             sage: A = Automaton(d, ['p'],['q'])
-            
+
         ::
-        
+
             sage: B = A.minimal_automaton()
             sage: B
             Automaton of 2 states
             sage: B._states
-            {0, 1}            
+            {0, 1}
 
         ::
-        
+
             sage: C = A.minimal_automaton(algorithm="Brzozowski")
             sage: C
             Automaton of 2 states
             sage: C._states
             {0, 1}
-            
+
         ::
-        
+
             sage: D = A.minimal_automaton(algorithm="Moore")
             sage: D
             Automaton of 2 states
             sage: D._states
-            {0, 1}    
-        
+            {0, 1}
 
-        """        
+
+        """
         if (algorithm is None) or (algorithm == "Hopcroft"):
             R = self._minimal_automaton_hopcroft()
         elif algorithm == "Moore":
             their = self.to_theirs()
-            their = their.determinisation()            
+            their = their.determinisation()
             M = their.minimization(algorithm=algorithm)
             R = Automaton.from_theirs(M)
         elif algorithm == "Brzozowski":
@@ -804,18 +804,18 @@ class Automaton(object):
             M = their.minimization(algorithm=algorithm)
             R = Automaton.from_theirs(M)
         else:
-            raise NotImplementedError("Algorithm '"+algorithm+"' is not implemented. Choose 'Hopcroft' or 'Moore' or 'Brzozowski'")   
+            raise NotImplementedError("Algorithm '"+algorithm+"' is not implemented. Choose 'Hopcroft' or 'Moore' or 'Brzozowski'")
         if rename_states:
             R.rename_states()
-        return R  
-    
+        return R
+
     def _minimal_automaton_hopcroft(self):
         r"""
         Minimize the automaton self
 
         EXAMPLES::
 
-            
+
 
         """
         AD = self.deterministic_automaton(rename_states=True)
@@ -848,11 +848,11 @@ class Automaton(object):
                                 if len(Y.intersection(X)) <= len(Y - X):
                                     W.add(frozenset(Y.intersection(X)))
                                 else :
-                                    W.add(frozenset(Y-X))                
+                                    W.add(frozenset(Y-X))
                     P= copy(Pp)
-        states =[]       
+        states =[]
         for i in P:
-            if not (i==set([])):                  
+            if not (i==set([])):
                 states.append(tuple(i))
         transitions =  {}
         for i in states:
@@ -860,7 +860,7 @@ class Automaton(object):
                 p = AD._transitions[(i[0],j)]
                 for k in states:
                     if k.count(p[0]) > 0:
-                        transitions[(i,j)] = [k]                          
+                        transitions[(i,j)] = [k]
         final_states = []
         initial_states = []
         for i in states:
@@ -921,7 +921,7 @@ class Automaton(object):
         boolean
 
         EXAMPLES::
-        
+
             sage: from pysemigroup import Automaton
             sage: d = {(0, 'a'): [1], (1, 'a'): [0]}
             sage: B = Automaton(d,[0],[1])
@@ -929,7 +929,7 @@ class Automaton(object):
             False
             sage: B.is_equivalent("aa","aaaa")
             True
-                
+
         """
 
         d_u = self.word_to_transitions(u)
@@ -942,9 +942,9 @@ class Automaton(object):
                 if ((i in d_u) and not (i in d_v)) or ((i in d_v) and not (i in d_u)):
                     return False
         return True
-    def graphviz_string(self): 
+    def graphviz_string(self):
         r"""
-        Return graphviz representation of self. 
+        Return graphviz representation of self.
         INPUT :
         -  ``self`` -  Automaton
 
@@ -953,16 +953,16 @@ class Automaton(object):
         string
 
         EXAMPLES::
-        
+
             sage: from pysemigroup import Automaton
             sage: d = {(0, 'a'): [1], (1, 'a'): [0]}
             sage: B = Automaton(d,[0],[1])
             sage: B.graphviz_string()
             'digraph {\n ranksep=0.5;\n d2tdocpreamble = "\\usetikzlibrary{automata}";\n d2tfigpreamble = "\\tikzstyle{every state}= [ draw=blue!50,very thick,fill=blue!20]  \\tikzstyle{auto}= [fill=white]";\n node [style="state"];\n edge [lblstyle="auto",topath="bend right", len=4  ]\n  "0" [label="0",style = "state, initial"];\n  "1" [label="1",style = "state, accepting"];\n  "0" -> "1" [label="a"];\n  "1" -> "0" [label="a"];\n}'
-                
+
         """
 
-        s = 'digraph {\n node [margin=0 shape=circle style=filled]\n edge [len =2]\n' 
+        s = 'digraph {\n node [margin=0 shape=circle style=filled]\n edge [len =2]\n'
         for x in self._states:
             s = s+'  "'+str(x)+'" [label="'+str(x)+'" '
             if x in self._initial_states:
@@ -975,10 +975,10 @@ class Automaton(object):
                     s = s+'shape = doublecircle fillcolor = lightblue '
                 else:
                     s = s+' fillcolor = lightblue'
-                    
-                   
+
+
             s = s+"];\n"
-        for x in self._states:        
+        for x in self._states:
             for y in self._states:
                 edge = []
                 for a in self._alphabet:
@@ -994,16 +994,16 @@ class Automaton(object):
                     s = s + '];\n'
         s = s+'}'
         return s
-    
+
     def to_dot(self, file_name):
         r"""
-        Save to filename a dot file representing the automata. 
+        Save to filename a dot file representing the automata.
         INPUT :
         -  ``self`` -  Automaton
-        -  ``file_name`` -  string        
+        -  ``file_name`` -  string
         """
         s = self.graphviz_string()
-        f = open(file_name+".dot",'w')  
+        f = open(file_name+".dot",'w')
         f.write(s)
         f.close()
 
@@ -1012,12 +1012,12 @@ class Automaton(object):
         Save to filename a gif file representing the automata obtained with prog (dot neato circo).
         INPUT :
         -  ``self`` -  Automaton
-        -  ``file_name`` -  string 
-        -  ``prog`` -  string          
+        -  ``file_name`` -  string
+        -  ``prog`` -  string
         """
 
         self.to_dot(file_name)
-        if prog in ["dot","circo","neato"]:              
+        if prog in ["dot","circo","neato"]:
             os.system(prog+' -Tgif  '+file_name+'.dot -o'+file_name+'.gif')
             os.system('rm '+file_name+'.dot')
         else:
@@ -1029,12 +1029,12 @@ class Automaton(object):
         Save to filename a gif file representing the automata obtained with prog (dot neato circo).
         INPUT :
         -  ``self`` -  Automaton
-        -  ``file_name`` -  string 
-        -  ``prog`` -  string          
+        -  ``file_name`` -  string
+        -  ``prog`` -  string
         """
- 
+
         self.to_dot(file_name)
-        if prog in ["dot","circo","neato"]:              
+        if prog in ["dot","circo","neato"]:
             os.system(prog+' -Tsvg  '+file_name+'.dot -o'+file_name+'.svg')
             os.system('rm '+file_name+'.dot')
         else:
@@ -1052,18 +1052,18 @@ class Automaton(object):
                         d[(x,y)]=1
                     else:
                         d[(x,y)]=0
-            return RingMatrix((len(self._states),len(self._states)),d)        
+            return RingMatrix((len(self._states),len(self._states)),d)
         raise ValueError("Automaton type"+self._type+" is unsupported yet")
 
-        
-    def letter_to_algebra(self,letter,verbose=True):
+
+    def letter_to_algebra(self, letter, verbose=True):
         r"""
         return a matrix representing letter action on state.
         The return type depend on the choosed of aut_type in init. Only work
         for boolean and buchi type.
         INPUT :
         -  ``self`` -  Automaton
-        -  ``letter`` -  string 
+        -  ``letter`` -  string
         OUTPUT :
         - an object representing matrix on adequate ring
         """
@@ -1079,13 +1079,13 @@ class Automaton(object):
                     else:
                         Lx.append(0)
                 L.append(Lx)
-            return hash_matrix(L)
+            return hash_matrix(L, dtype=bool)
         if self._type == "buchi":
             d = {}
-            states = self._states       
+            states = self._states
             for y in states:
                 for z in states:
-                    d[(y,z)] = "-oo"            
+                    d[(y,z)] = "-oo"
                     if ((y,letter) in self._transitions) and (z in self._transitions[(y,letter)]):
                         if (z in self._final_states):
                             d[(y,z)] = 1
@@ -1093,7 +1093,7 @@ class Automaton(object):
                             d[(y,z)] = 0
                     else:
                         d[(y,z)] = "-oo"
-            fctx = BuchiMatrix((self._states,self._states),d)            
+            fctx = BuchiMatrix((self._states,self._states),d)
             return fctx
         raise ValueError("Automaton type"+self._type+" is unsupported yet")
 
